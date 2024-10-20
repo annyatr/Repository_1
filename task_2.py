3aa2654031c707967ada680391e4c646b4ff9061
annyatr <163312011+annyatr@users.noreply.github.com>
2024-10-20 22:27:50 +0300

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,24 @@
+# TODO Найдите количество книг, которое можно разместить на дискете
+
+
+disk_size_mb = 1.44
+pages = 100
+lines_per_page = 50
+chars_per_line = 25
+bytes_per_char = 4
+
+
+total_chars = pages * lines_per_page * chars_per_line
+
+
+book_size_bytes = total_chars * bytes_per_char
+
+
+disk_size_bytes = disk_size_mb * 1024 * 1024
+
+
+books_on_disk = disk_size_bytes // book_size_bytes
+
+
+print("Количество книг, помещающихся на дискету:", int(books_on_disk))
+
